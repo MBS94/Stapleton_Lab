8d66ef88cd3a026d1bf381a76b8ac527776c04c1
Michael Byrd <mcb4548@uncw.edu>
2018-02-05 16:34:26 -0500

    Reading in the data. Changing the values to appropriate data type. Create effected list of SNP.

--- gxeProject.py
+++ gxeProject.py
@@ -1,17 +1,30 @@
-import pandas as pd
 import random
 
-import simuPOP
+# import simuPOP
 
-data = pd.read_csv("ManchingScrubbed.csv")
+data = []
 
-# print(data.head())
+with open("ManchingScrubbed.csv", 'r') as f:
+    for line in f:
+        data.append(line.strip().split(','))
 
-numSNPS = 3235
+for i in range(3, len(data)):
+    data[i][0] = int(data[i][0])
 
-colNumList = []
+SNP_List = data[0][4:]
 
-for i in range(1, numSNPS + 1):
-    colNumList.append(i)
+random.shuffle(SNP_List)
+effect_SNP = SNP_List[:1000]
 
-random.shuffle(colNumList)
+correct_SNP_Value = "\"A\""
+
+i = 1
+
+for i in range(len(data)):
+    for j in range(len(data[0])):
+        # print(data[i][j])
+        if data[i][j] == correct_SNP_Value:# and data[0][j] in effect_SNP:
+            print("Yes")
+
+# for i in range(10):
+#     print(data[i])
