51619f9bded638b06d0fb1b750854b2377b8304a
Michael Byrd <mbyrdsmith94@gmail.com>
2019-03-13 14:00:33 -0400

    Script correcting

--- j_gray/submit.sh
+++ j_gray/submit.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-#SBATCH -J FamilyRandomPractice        # Job name
+
+#SBATCH -J Practice_Job_18_Dec          # Job name
 #SBATCH -e myjob.e%j       # Name of stderr error file
-#SBATCH -p normal          # Queue (partition) name
+#SBATCH -p skx-normal          # Queue (partition) name
 #SBATCH -N 1               # Total # of nodes (must be 1 for serial)
 #SBATCH -n 1               # Total # of mpi tasks (should be 1 for serial)
 #SBATCH -t 48:00:00        # Run time (hh:mm:ss)
 #SBATCH --mail-user=mcb4548@uncw.edu
 #SBATCH --mail-type=all    # Send email at begin and end of job
 
+
 # Other commands must follow all #SBATCH directives...
 
-Rscript --vanilla --verbose ./practicewithQTL.R > ./output.Rout
+mkdir output
+Rscript --vanilla --verbose ./practivewithQTL.R > ./output.Rout
