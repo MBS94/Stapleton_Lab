e2ddec57c5262a9b4f2351df45a92c0da385f9a3
Michael Byrd <mbyrd@Michaels-MacBook-Pro.local>
2019-06-03 10:33:41 -0400

    Running interactive and additive scanonevar

--- vQTL/Manching_Covariate/Interaction/2019_06_vQTLManching.R
+++ vQTL/Manching_Covariate/Interaction/2019_06_vQTLManching.R
@@ -2,6 +2,10 @@
 #install.packages("qtl")
 library(qtl)
 library(vqtl)
+library(purrr)
+library(readr)
+library(dplyr)
+library(tidyverse)
 setwd("/work/04908/mcb4548/stampede2/GitHub/Stapleton_Lab/vQTL/Manching_Covariate/Interaction")
 # Mac Path
 #setwd("/Users/mbyrd/Stapleton/Stapleton_Lab/vQTL/Manching_Covariate/Interaction")
@@ -10,9 +14,19 @@ fr <- drop.nullmarkers(fr)
 #scan with variance
 fr <- calc.genoprob(fr)
 
-outv <- scanonevar(cross = fr,
+addOneVar <- scanonevar(cross = fr,
                    mean.formula = Height ~ Env + mean.QTL.add + mean.QTL.dom,
                    var.formula = ~ Env + var.QTL.add + var.QTL.dom,
                    return.covar.effects = TRUE)
 
-write_rds(outv, "2019_06_outv.rds", compress = "xz")
+write_rds(outv, "addOneVar.rds", compress = "xz")
+
+intOneVar <- scanonevar(cross = sample,
+                   mean.formula = Height ~ Env * (mean.QTL.add + mean.QTL.dom),
+                   var.formula = ~ Env * (var.QTL.add + var.QTL.dom),
+                   return.covar.effects = TRUE)
+
+write_rds(outv, "intOneVar.rds", compress = "xz")
+
+write.csv(outv$result, file = "Manching_additive_model.csv")
+write.csv(outv$result, file = "Manching_interactive_model.csv")
