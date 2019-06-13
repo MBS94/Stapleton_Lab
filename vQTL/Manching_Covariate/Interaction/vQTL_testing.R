a73d72de67ee7ca6b0bf8d012d4eba862e254a0e
Michael Byrd <mbyrd@Michaels-MacBook-Pro.local>
2019-06-13 13:13:41 -0400

    Testing new sov

---
+++ vQTL/Manching_Covariate/Interaction/vQTL_testing.R
@@ -0,0 +1,59 @@
+# Loading the packages
+library(qtl)
+library(vqtl)
+library(purrr)
+library(readr)
+library(dplyr)
+library(tidyverse)
+
+# Setting working directories
+# Comment out whichever one is not being used.
+
+# Stampede2 Directory
+setwd("/work/04908/mcb4548/stampede2/GitHub/Stapleton_Lab/vQTL/Manching_Covariate/Interaction")
+
+# Michael's Mac Directory
+# setwd("/Users/mbyrd/Stapleton/Stapleton_Lab/vQTL/Manching_Covariate/Interaction")
+
+# Reading in the input file as a 'cross' object
+
+# data <- read.csv(file = "ManchingStressData_Covar.csv")
+
+# Created a random sample
+# set.seed(1234)
+# subset = data[c(1,2, round(runif(300,1,6674))),]
+# write.csv(subset, file = "Manching_Sample300.csv")
+
+
+# Full Data Set, Comment if using Sample
+fr <-read.cross(file = "ManchingStressData_Covar.csv")
+
+# Sample of Data Set, Comment if using full data
+# fr <-read.cross(file = "ManchingStressData_Covar.csv")
+
+# Not sure what these two functions do yet.
+fr <- drop.nullmarkers(fr)
+#scan with variance
+fr <- calc.genoprob(fr)
+
+fr$pheno$Env <- factor(fr$pheno$Env)
+print("before scanonevar")
+
+#Height ~ Env + mean.QTL.add + mean.QTL.dom + Env * (mean.QTL.add + mean.QTL.dom)
+# ~ Env + var.QTL.add + var.QTL.dom + Env * (var.QTL.add + var.QTL.dom)
+
+# Interactive scanonevar function
+test_sov <- scanonevar(cross = fr,
+                        mean.formula = Height ~ Env + mean.QTL.add + mean.QTL.dom + Env * (mean.QTL.add + mean.QTL.dom),
+                        var.formula = ~ Env + var.QTL.add + var.QTL.dom + Env * (var.QTL.add + var.QTL.dom),
+                        return.covar.effects = TRUE)
+print("after scanonevar")
+
+# Writing the result of the interactive scanonevar for later use
+write_rds(test_sov, "test_sov.rds", compress = "xz")
+
+
+# Writing out the results of the two 
+write.csv(test_sov$result, file = "test_sov_result.csv")
+
+
